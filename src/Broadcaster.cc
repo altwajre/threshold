f98534b3377f86b561e962019e8d0a5a8f5a9c8d
Andreas C. Osowski <andreas@osowski.de>
2019-01-21 22:19:41 +0100

    clang format

--- src/Broadcaster.cc
+++ src/Broadcaster.cc
@@ -3,15 +3,15 @@
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+//
 
 #include <omnetpp.h>
 
@@ -20,21 +20,20 @@ using namespace omnetpp;
 /**
  * TODO - Generated class
  */
-class Broadcaster: public cSimpleModule {
-protected:
-    virtual void initialize();
-    virtual void handleMessage(cMessage *msg);
+class Broadcaster : public cSimpleModule {
+ protected:
+  virtual void initialize();
+  virtual void handleMessage(cMessage *msg);
 };
 
 Define_Module(Broadcaster);
 
-void Broadcaster::initialize() {
-}
+void Broadcaster::initialize() {}
 
 void Broadcaster::handleMessage(cMessage *msg) {
-    size_t outCount = gateSize("out");
-    size_t outBase = gateBaseId("out");
-    for (size_t i = 0; i < outCount; i++) {
-        send(i == (outCount - 1) ? msg : msg->dup(), outBase + i);
-    }
+  size_t outCount = gateSize("out");
+  size_t outBase = gateBaseId("out");
+  for (size_t i = 0; i < outCount; i++) {
+    send(i == (outCount - 1) ? msg : msg->dup(), outBase + i);
+  }
 }
--- src/Generator.cc
+++ src/Generator.cc
@@ -3,85 +3,79 @@
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+//
 
 #include <omnetpp.h>
 #include "Packet_m.h"
 
 using namespace omnetpp;
 
-class Generator: public cSimpleModule {
-private:
-    double chanceGeneratePkt;
-    double chancePktIsSpam;
+class Generator : public cSimpleModule {
+ private:
+  double chanceGeneratePkt;
+  double chancePktIsSpam;
 
-    // state
-    cPar* generationInterval;
-    cMessage *generatePacket;
-    int myAddress;
-    long pktCounter;
+  // state
+  cPar *generationInterval;
+  cMessage *generatePacket;
+  int myAddress;
+  long pktCounter;
 
-public:
-    Generator();
-    virtual ~Generator();
+ public:
+  Generator();
+  virtual ~Generator();
 
-protected:
-    virtual void initialize() override;
-    virtual void handleMessage(cMessage *msg) override;
+ protected:
+  virtual void initialize() override;
+  virtual void handleMessage(cMessage *msg) override;
 };
 
 Define_Module(Generator);
 
 Generator::Generator() {
-    generatePacket = nullptr;
+  generatePacket = nullptr;
 
-    pktCounter = 0;
+  pktCounter = 0;
 }
 
-Generator::~Generator() {
-    cancelAndDelete(generatePacket);
-
-}
+Generator::~Generator() { cancelAndDelete(generatePacket); }
 
 void Generator::initialize() {
-    myAddress = par("address");
-    generationInterval = &par("generationInterval");
-    chanceGeneratePkt = par("chanceGeneratePkt");
-    chancePktIsSpam = par("chancePktIsSpam");
+  myAddress = par("address");
+  generationInterval = &par("generationInterval");
+  chanceGeneratePkt = par("chanceGeneratePkt");
+  chancePktIsSpam = par("chancePktIsSpam");
 
-    generatePacket = new cMessage("generatePacket");
-    scheduleAt(generationInterval->doubleValue(), generatePacket);
+  generatePacket = new cMessage("generatePacket");
+  scheduleAt(generationInterval->doubleValue(), generatePacket);
 }
 
 void Generator::handleMessage(cMessage *msg) {
-    if (msg == generatePacket) {
-
-        if (uniform(0, 1) >= chanceGeneratePkt) {
-            return;
-        }
-
-        char pktID[42];
-        sprintf(pktID, "pk-%d-#%ld", myAddress, pktCounter++);
-
-        Packet *pk = new Packet(pktID);
-        pk->setSrcAddr(myAddress);
-
-        // Broadcast signal to neighbours.
-        send(pk, "out");
-
-        scheduleAt(simTime() + generationInterval->doubleValue(),
-                generatePacket);
-        if (hasGUI()) {
-            getParentModule()->bubble("Generating packet...");
-        }
+  if (msg == generatePacket) {
+    if (uniform(0, 1) >= chanceGeneratePkt) {
+      return;
     }
 
+    char pktID[42];
+    sprintf(pktID, "pk-%d-#%ld", myAddress, pktCounter++);
+
+    Packet *pk = new Packet(pktID);
+    pk->setSrcAddr(myAddress);
+
+    // Broadcast signal to neighbours.
+    send(pk, "out");
+
+    scheduleAt(simTime() + generationInterval->doubleValue(), generatePacket);
+    if (hasGUI()) {
+      getParentModule()->bubble("Generating packet...");
+    }
+  }
 }
